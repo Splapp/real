faacefe55e25df1c6b1f95aaeb43110f71d85ed0
G <G@someplace>
2015-06-30 15:27:40 -0800

    Im commited

--- src/com/example/proj/MainActivity.java
+++ src/com/example/proj/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		//gabriel was here
+		//fuck n an activity
 		
 		
 		
